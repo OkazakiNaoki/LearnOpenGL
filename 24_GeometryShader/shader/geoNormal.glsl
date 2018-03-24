16e4c71a5645bf4e947e1c55dd4ef069fed42aff
OkazakiNaoki <deanwinchester0326@gmail.com>
2018-03-25 03:26:11 +0800

    Fix normal line rotate problem

--- 24_GeometryShader/shader/geoNormal.glsl
+++ 24_GeometryShader/shader/geoNormal.glsl
@@ -3,22 +3,16 @@ layout (triangles) in;
 layout (line_strip, max_vertices = 6) out;
 
 in VS_OUT {
-    vec3 normal;
-    mat4 projection;
-    mat4 view;
-    mat4 model;
+    vec4 normal;
 } gs_in[];
 
-const float MAGNITUDE = 0.2;
+const float MAGNITUDE = 0.4;
 
 void GenerateLine(int index)
 {
     gl_Position = gl_in[index].gl_Position;
     EmitVertex();
-    // for some reason, normal transform will not perform correctly during vertex shader stage, so we move the calculation to here
-    mat4 normalMatrix = mat4(mat3(transpose(inverse(gs_in[index].model))));
-    vec4 normalVec = normalize(gs_in[index].projection * gs_in[index].view * normalMatrix * vec4(gs_in[index].normal, 0.0f));
-    gl_Position = gl_in[index].gl_Position + normalVec * MAGNITUDE;
+    gl_Position = gl_in[index].gl_Position + gs_in[index].normal * MAGNITUDE;
     EmitVertex();
     EndPrimitive();
 }
--- 24_GeometryShader/shader/vertexNormal.glsl
+++ 24_GeometryShader/shader/vertexNormal.glsl
@@ -3,10 +3,7 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
 
 out VS_OUT {
-    vec3 normal;
-	mat4 projection;
-	mat4 view;
-	mat4 model;
+    vec4 normal;
 } vs_out;
 
 uniform mat4 projection;
@@ -15,9 +12,7 @@ uniform mat4 model;
 
 void main()
 {
-    vs_out.normal = aNormal;
-    vs_out.projection = projection;
-    vs_out.view = view;
-    vs_out.model = model;
     gl_Position = projection * view * model * vec4(aPos, 1.0); 
+    mat4 normalMatrix = transpose(inverse(model));
+    vs_out.normal = normalize(projection * view * normalMatrix * vec4(aNormal, 0.0));
 }
