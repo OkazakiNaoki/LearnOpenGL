5830e38faacf6426fa067915b2d07affecb03d13
OkazakiNaoki <deanwinchester0326@gmail.com>
2018-03-25 15:37:29 +0800

    Add anti-aliasing

---
+++ 26_AntiAliasing/ModelLoader.cpp
@@ -0,0 +1,163 @@
+#include "header/ModelLoader.h"
+#include <gtc/matrix_transform.hpp>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <map>
+
+ModelLoader::ModelLoader(std::string const &path, bool gamma) : gammaCorrection(gamma)
+{
+	loadModel(path);
+}
+
+ModelLoader::ModelLoader(std::string const &path, TexLoader* manual, bool gamma) : gammaCorrection(gamma), manualTexture(manual)
+{
+	loadModel(path);
+}
+
+void ModelLoader::Draw(GLSLloader glslLoader)
+{
+	for (unsigned int i = 0; i < meshes.size(); i++)
+		meshes[i].Draw(glslLoader);
+}
+
+void ModelLoader::loadModel(std::string const &path)
+{
+	Assimp::Importer importer;
+	const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_CalcTangentSpace);
+
+	if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
+	{
+		std::cout << "ERROR::ASSIMP:: " << importer.GetErrorString() << std::endl;
+		return;
+	}
+	// retrieve the directory path of the filepath
+	directory = path.substr(0, path.find_last_of('/'));
+
+	processNode(scene->mRootNode, scene);
+}
+
+void ModelLoader::processNode(aiNode *node, const aiScene *scene)
+{
+	// current node
+	for (unsigned int i = 0; i < node->mNumMeshes; i++)
+	{
+		aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+		meshes.push_back(processMesh(mesh, scene));
+	}
+	// recursive child nodes 
+	for (unsigned int i = 0; i < node->mNumChildren; i++)
+	{
+		processNode(node->mChildren[i], scene);
+	}
+
+}
+
+Mesh ModelLoader::processMesh(aiMesh *mesh, const aiScene *scene)
+{
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+	std::vector<Texture> textures;
+
+	for (unsigned int i = 0; i < mesh->mNumVertices; i++)
+	{
+		Vertex vertex;
+		glm::vec3 vector;
+		// positions
+		vector.x = mesh->mVertices[i].x;
+		vector.y = mesh->mVertices[i].y;
+		vector.z = mesh->mVertices[i].z;
+		vertex.Position = vector;
+		// normals
+		vector.x = mesh->mNormals[i].x;
+		vector.y = mesh->mNormals[i].y;
+		vector.z = mesh->mNormals[i].z;
+		vertex.Normal = vector;
+		// texture coordinates
+		if (mesh->mTextureCoords[0])
+		{
+			glm::vec2 vec;
+			vec.x = mesh->mTextureCoords[0][i].x;
+			vec.y = mesh->mTextureCoords[0][i].y;
+			vertex.TexCoords = vec;
+		}
+		else
+			vertex.TexCoords = glm::vec2(0.0f, 0.0f);
+		// tangent
+		vector.x = mesh->mTangents[i].x;
+		vector.y = mesh->mTangents[i].y;
+		vector.z = mesh->mTangents[i].z;
+		vertex.Tangent = vector;
+		// bitangent
+		vector.x = mesh->mBitangents[i].x;
+		vector.y = mesh->mBitangents[i].y;
+		vector.z = mesh->mBitangents[i].z;
+		vertex.Bitangent = vector;
+		vertices.push_back(vertex);
+	}
+	for (unsigned int i = 0; i < mesh->mNumFaces; i++)
+	{
+		aiFace face = mesh->mFaces[i];
+		for (unsigned int j = 0; j < face.mNumIndices; j++)
+			indices.push_back(face.mIndices[j]);
+	}
+	// process materials
+	aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
+
+	std::vector<Texture> diffuseMaps = loadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
+	textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
+	std::vector<Texture> specularMaps = loadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
+	textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
+	std::vector<Texture> normalMaps = loadMaterialTextures(material, aiTextureType_HEIGHT, "texture_normal");
+	textures.insert(textures.end(), normalMaps.begin(), normalMaps.end());
+	std::vector<Texture> heightMaps = loadMaterialTextures(material, aiTextureType_AMBIENT, "texture_height");
+	textures.insert(textures.end(), heightMaps.begin(), heightMaps.end());
+
+	return Mesh(vertices, indices, textures);
+}
+
+std::vector<Texture> ModelLoader::loadMaterialTextures(aiMaterial *mat, aiTextureType type, std::string typeName)
+{
+	std::vector<Texture> textures;
+	if (manualTexture == NULL) {
+		if (mat->GetTextureCount(type) > 0) {
+			for (unsigned int i = 0; i < mat->GetTextureCount(type); i++)
+			{
+				aiString str;
+				mat->GetTexture(type, i, &str);
+				bool skip = false;
+				for (unsigned int j = 0; j < textures_loaded.size(); j++)
+				{
+					if (std::strcmp(textures_loaded[j].path.data(), str.C_Str()) == 0)
+					{
+						textures.push_back(textures_loaded[j]);
+						skip = true;
+						break;
+					}
+				}
+				std::cout << skip << std::endl;
+				if (!skip)
+				{
+					Texture texture;
+					std::string filename = this->directory + '/' + std::string(str.C_Str());
+					const char *cstr = filename.c_str();
+					TexLoader texLoader(cstr, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR);
+					texture.id = texLoader.GetMapID();
+					texture.type = typeName;
+					texture.path = str.C_Str();
+					textures.push_back(texture);
+					textures_loaded.push_back(texture);
+				}
+			}
+		}
+	}
+	else{
+		Texture texture;
+		texture.id = manualTexture->GetMapID();
+		texture.type = typeName;
+		//texture.path = str.C_Str();
+		textures.push_back(texture);
+		textures_loaded.push_back(texture);
+	}
+	return textures;
+}
---
+++ 26_AntiAliasing/shader/fragRed.glsl
@@ -0,0 +1,13 @@
+#version 330 core
+out vec4 FragColor;
+
+in VS_OUT // name should be same as vertex "VS_OUT"
+{
+    vec2 TexCoord;
+	vec3 Position;
+} fs_in;
+
+void main()
+{
+    FragColor = vec4(0.8, 0.0, 0.0, 1.0);
+}
