723975be6c280799f0a039c1773e452abedfa20f
OkazakiNaoki <deanwinchester0326@gmail.com>
2018-04-23 11:16:32 +0800

    Add parallax occlusion mapping

--- 32_ParallaxMapping/shader/fragLight.glsl
+++ 32_ParallaxMapping/shader/fragLight.glsl
@@ -28,7 +28,7 @@ vec2 ParallaxMapping(vec2 texCoords, vec3 viewDir)
 
     // number of depth layers
     const float minLayers = 8.0;
-	const float maxLayers = 200.0;
+	const float maxLayers = 64.0;
 	float numLayers = mix(maxLayers, minLayers, abs(dot(vec3(0.0, 0.0, 1.0), viewDir)));
 	// calculate the size of each layer
     float layerDepth = 1.0 / numLayers;
@@ -52,7 +52,19 @@ vec2 ParallaxMapping(vec2 texCoords, vec3 viewDir)
 	    currentLayerDepth += layerDepth;  
 	}
 
-	return currentTexCoords;
+	// Parallax Occlusion Mapping part
+	// get texture coordinates before collision (reverse operations)
+    vec2 prevTexCoords = currentTexCoords + deltaTexCoords;
+
+    // get depth after and before collision for linear interpolation
+    float afterDepth  = currentDepthMapValue - currentLayerDepth;
+    float beforeDepth = texture(depthMap, prevTexCoords).r - currentLayerDepth + layerDepth;
+ 
+    // interpolation of texture coordinates
+    float weight = afterDepth / (afterDepth - beforeDepth);
+    vec2 finalTexCoords = prevTexCoords * weight + currentTexCoords * (1.0 - weight);
+
+    return finalTexCoords;
 }
 
 void main()
